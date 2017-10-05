59eb5ce3505a5baf41af59ca4e154b93eeac1bf4
Igor Soloydenko <soloydenko@gmail.com>
2017-10-05 00:30:44 -0700

    v0.0.1 release preparation

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { Authentication } from './authentication';
+export { CrossOriginAuthentication } from './cross-origin-authentication';
+export { DbConnection } from './db-connection';
+export { Management } from './management';
+export { PasswordlessAuthentication } from './passwordless-authentication';
+export { Popup } from './popup';
+export { Redirect } from './redirect';
+export { WebAuth } from './web-auth';
